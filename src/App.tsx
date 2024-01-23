8b2c2057892e77a475879aa95231d8e4d1292c67
MSuhail <owais0203@gmail.com>
2024-01-23 14:38:04 +0100

    Routes & File & Folder Structures

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
 import './globals.css';
+
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import { Home } from './_root/pages';
+
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello Snapgram!
-    </h1>
+    <main className="flex h-screen">
+      <Routes>
+        {/* public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* private Routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   )
 }
 

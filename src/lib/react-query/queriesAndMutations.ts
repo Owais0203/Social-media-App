b03243bf2303ec200891fdee320a1933a7771a47
MSuhail <owais0203@gmail.com>
2024-01-26 23:30:58 +0100

    More degugging

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -155,6 +155,7 @@ export const useGetPosts = () => {
       const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
       return lastId;
     },
+    initialPageParam: () => {},
   });
 };
 

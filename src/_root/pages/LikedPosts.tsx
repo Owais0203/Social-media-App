a58442fcd3092c7eb2a60e3f4ad0476ebbf61e6e
MSuhail <owais0203@gmail.com>
2024-01-29 15:13:45 +0100

    Liked-Post page functionality

--- src/_root/pages/LikedPosts.tsx
+++ src/_root/pages/LikedPosts.tsx
@@ -1,7 +1,28 @@
+import GridPostList from "@/components/shared/GridPostList";
+import Loader from "@/components/shared/Loader";
+import { useGetCurrentUser } from "@/lib/react-query/queriesAndMutations"
+
+
 const LikedPosts = () => {
+  const { data: currentUser } = useGetCurrentUser();
+
+  if(!currentUser) {
+    return (
+      <div>
+        <Loader />
+      </div>
+    );
+  }
+
   return (
-    <div>LikedPosts</div>
-  )
-}
+    <>
+      {currentUser.liked.length === 0 && (
+        <p className="text-light-4">No liked posts</p>
+      )}
+
+      <GridPostList posts={currentUser.liked} showStats={false} />
+    </>
+  );
+};
 
 export default LikedPosts
